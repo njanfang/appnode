6c8a7077ccf73923e568d82bfd57ef6e23497c14
njanfang <njanfang@gmail.com>
2024-12-25 16:52:07 +0100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Your entry file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  mode: 'development'
+};
