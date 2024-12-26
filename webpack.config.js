068c6628a954f745020865e2748b5b94112f5540
njanfang <njanfang@gmail.com>
2024-12-26 09:59:46 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,6 @@
 const path = require('path');
 
 module.exports = {
-      fallback: {
-      "zlib": require.resolve("browserify-zlib"),
-      "querystring": require.resolve("querystring-es3"),
-      "path": require.resolve("path-browserify"),
-      "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "fs": false, // fs is typically not needed in the browser environment
-    }
-
 
   
   entry: './app.js', // Your entry file
