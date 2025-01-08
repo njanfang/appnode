20558f30b3dc29dbc811d6cd4fc03dac766975c3
njanfang <njanfang@gmail.com>
2025-01-08 23:14:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();app.get('/', function (req, res) {
-  res.send('Hello World Njanfang!');
+  res.send('Hello World1 Njanfang!');
 });app.listen(1020, function () {
   console.log('Example app listening on port 1020!');
 });
